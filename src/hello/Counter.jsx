de77c053d1bb2ac51cb637e6733332f4f1c97be8
leedo <leedoyun98@naver.com>
2021-01-19 15:56:09 +0900

    first

---
+++ src/hello/Counter.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+const Counter = () =>{
+    const [number, setNumber] = useState(0)
+    return <>
+    <h1>카운터</h1>
+    <h2>{number}</h2>
+    <button onClick = {()=> setNumber(number +1)}>+</button>
+    <button onClick = {()=> setNumber(number -1)}>-</button>
+    </>
+}
+
+export default Counter
---
+++ src/hello/Hello.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Hello = ()=>{
+    return <>
+    <h1> 반갑다 리액트 !!!!</h1>
+    </>
+}
+export default Hello
